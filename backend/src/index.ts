b2615eb3b4dd486823b8ff10474dfde67c6db065
nightshrine <yamamoto.shunsuke.20@shizuoka.ac.jp>
2024-11-21 08:42:02 +0900

    index.tsからprismaのメソッドは直接呼び出さないように

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,4 @@
 import { Hono } from 'hono';
-import { PrismaClient } from '@prisma/client';
-import { PrismaD1 } from '@prisma/adapter-d1';
 import { corsMiddleware } from './middleware/cors';
 
 type Bindings = {
@@ -12,11 +10,7 @@ const app = new Hono<{ Bindings: Bindings }>();
 app.use('*', corsMiddleware('*'));
 
 app.get('/', async (c) => {
-    const adapter = new PrismaD1(c.env.DB);
-    const prisma = new PrismaClient({ adapter });
-
-    const sample = await prisma.sample.findFirst();
-    return c.json(sample);
+    return c.json({ name: 'Hello, World!' });
 });
 
 export default app;
